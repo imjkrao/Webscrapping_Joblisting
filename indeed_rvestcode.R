bd5072c45f0e84250688785f5196edbf7e16ec1a
Jeevankrishna <raojk33@gmail.com>
2020-03-31 21:48:44 +0100

    .v2 to Scrape multiple pages > added comments > Changed wild card Note: Before scarapping, check for xpath class in website.

--- indeed_rvestcode.R
+++ indeed_rvestcode.R
@@ -1,10 +1,12 @@
-page_result_start <- 10 # starting page 
-page_result_end <- 210 # last page results
+library(rvest)
+library(xml2)
+page_result_start <- 10 # second page num in url 
+page_result_end <- 210 # last page num in url
 page_results <- seq(from = page_result_start, to = page_result_end, by = 10)
 
 full_df <- data.frame()
 for(i in seq_along(page_results)) {
-  
+  #search filtered to ireland, Data analyst and full time as Job type  
   first_page_url <- "https://ie.indeed.com/jobs?q=data+analyst&l=Ireland&rbl=Dublin&jlid=2bb3b9418219c336&jt=fulltime"
   url <- paste0(first_page_url, "&start=", page_results[i])
   page <- xml2::read_html(url)
@@ -13,7 +15,7 @@ for(i in seq_along(page_results)) {
   #get the job title
   job_title <- page %>% 
     rvest::html_nodes("div") %>%
-    rvest::html_nodes(xpath = '//a[@data-tn-element = "jobTitle"]') %>%
+    rvest::html_nodes(xpath = '//*[@data-tn-element = "jobTitle"]') %>%
     rvest::html_attr("title")
   
   #get the company name
