d837aeaf2d94e98cd48151b8f3fa6db32f245056
Jeevankrishna <raojk33@gmail.com>
2020-04-04 20:51:48 +0100

    New "first_page_url" for IT -ireland-fulltime

--- indeed_rvestcode.R
+++ indeed_rvestcode.R
@@ -1,13 +1,14 @@
 library(rvest)
 library(xml2)
 page_result_start <- 10 # second page num in url 
-page_result_end <- 210 # last page num in url
+page_result_end <- 410 # last page num in url
 page_results <- seq(from = page_result_start, to = page_result_end, by = 10)
 
 full_df <- data.frame()
 for(i in seq_along(page_results)) {
   #search filtered to ireland, Data analyst and full time as Job type  
-  first_page_url <- "https://ie.indeed.com/jobs?q=data+analyst&l=Ireland&rbl=Dublin&jlid=2bb3b9418219c336&jt=fulltime"
+  #first_page_url <- "https://ie.indeed.com/jobs?q=data+analyst&l=Ireland&rbl=Dublin&jlid=2bb3b9418219c336&jt=fulltime"
+  first_page_url <- "https://ie.indeed.com/jobs?q=IT&l=Ireland&jt=fulltime&"
   url <- paste0(first_page_url, "&start=", page_results[i])
   page <- xml2::read_html(url)
   Sys.sleep(2)
